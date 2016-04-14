5edc7cd94a2c2c74f8a13a8488e15628754f3e2b
Enrique Gonzalez <egonzalezh94@hotmail.com>
2016-04-14 11:12:40 -0700

    all info is being displayed on site. Basic layout has been set. Time to work on CSS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-//var weatherAPI = "http://api.aerisapi.com/observations/san+francisco,ca?client_id=EWh52qY2RwIHxKXEhGw9M&client_secret=SB3ZzcN8fAD2b4s7YVOCcXXNlm3nmY2R7BAbhdLK";
-
 var MUNI_API = "http://services.my511.org/Transit2.0/GetNextDeparturesByStopCode.aspx?token=db0ca4ba-fad8-40a0-9bb3-fd76564a83a9&stopcode=";
 
 var weatherAPI = "https://api.forecast.io/forecast/bab4dc22389dd9761e15b6a26e6b7615/37.8267,-122.423";
--- js/muni.js
+++ js/muni.js
@@ -12,26 +12,40 @@ busCodes["38In"] = "14270";
 busCodes["38Out"] = "14269";
 
 muni = function(){
+
     for (var busCode in busCodes) {
-          $.getJSON("controllers/muni.php?busCode="+busCodes[busCode],function(data) {
+
+
+          $.getJSON("controllers/muni.php?busCode=" + busCodes[busCode],function(data) {
+            $('.busStop').append("<p>New Code:</p>");
     	   var routeStops = data.AgencyList.Agency.RouteList.Route;
            for (var route in routeStops) {
-              var busName = routeStops[route]["@attributes"].Name;
-              var busDirection = routeStops[route].RouteDirectionList.RouteDirection["@attributes"].Name;
+              var _newHTML = "";
+              _newHTML += "<tr>"
+              var busName = routeStops[route]["@attributes"].Code;
+              var busDirection = routeStops[route].RouteDirectionList.RouteDirection["@attributes"].Code;
               var busStopStreet = routeStops[route].RouteDirectionList.RouteDirection.StopList.Stop["@attributes"].name;
               var busStopTimes = routeStops[route].RouteDirectionList.RouteDirection.StopList.Stop.DepartureTimeList.DepartureTime;
               var busStopTimeString = "";
+              _newHTML += '<td>' + busName + '</td>';
+              _newHTML += '<td>' + busDirection + '</td>';
+              _newHTML += '<td>' + busStopStreet + '</td>';
 
               if (busStopTimes != null) {
                 for (var busStopTime in busStopTimes) {
                     busStopTimeString += busStopTimes[busStopTime] + " ";
-                    
                 }
+                _newHTML += '<td>' + busStopTimeString + '</td>';
+              }
+            else { //Times are not available
 
-                console.log(busStopTimeString);
             }
+            _newHTML += "</tr>"
+            $('.busStop').append(_newHTML + "<br>");
         }
+
       });
+
     }
 
 };
